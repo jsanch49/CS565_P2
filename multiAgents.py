f9042fd3c56dd121822db3d385fd60e8cd6cbd77
Joseph Sanchez <jsanch49@binghamton.edu>
2021-10-13 15:01:20 -0400

    helpful prints

--- multiAgents.py
+++ multiAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -72,7 +72,10 @@ class ReflexAgent(Agent):
         newFood = successorGameState.getFood()
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-
+        print("newPos: ", newPos)
+        print("newFood: ", newFood)
+        print("newGhostStates: ", newGhostStates)
+        print("newScaredTimes: ", newScaredTimes)
         "*** YOUR CODE HERE ***"
         return successorGameState.getScore()
 
@@ -170,4 +173,3 @@ def betterEvaluationFunction(currentGameState):
 
 # Abbreviation
 better = betterEvaluationFunction
-
